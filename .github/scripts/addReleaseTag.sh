ac289e350456f7408e6ee869eda100b65d218fb8
nino01 <arunoa2@gmail.com>
2020-01-31 20:38:56 +0900

    chore: first commit

---
+++ .github/scripts/addReleaseTag.sh
@@ -0,0 +1,8 @@
+curl -s -X POST "https://api.github.com/repos/${REPO}/git/refs" \
+    -H "Authorization: token $GITHUB_TOKEN" \
+    -d @- << EOS
+{
+    "ref": "refs/tags/${TAG}",
+    "sha": "${COMMIT}"
+}
+EOS
